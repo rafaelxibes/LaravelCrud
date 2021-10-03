8b2748401714112cc6b046682eecf7623c093cbe
Rafaelxibes <rafaelxibes@hotmail.com>
2021-10-03 16:32:26 -0300

    Primero Commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+   
+   <!--======== CSS ======== -->
+   <link rel="stylesheet" href="css/styles.css">
+
+   <!-- BOXICONS -->
+   <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+
+   <title>@yield('title')</title>
+</head>
+<body>
+   @yield('content')
+   <!--============== FOOTER ============== -->
+   <footer>
+      <p>RafaelXibes Crud &copy;</p>
+   </footer>
+
+    <!--======== Scripts ======== -->
+   <script src="assets/js/scripts.js"></script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title', 'Crud')
+@section('content')
+
+<h1>Oi</h1>
+@endsection
