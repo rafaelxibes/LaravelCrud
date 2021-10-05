78ee05dd6412565bc0f96efa42e997fa31756ab9
Rafaelxibes <rafaelxibes@hotmail.com>
2021-10-04 22:54:57 -0300

    Terceira

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,20 @@
    <section class="container section section__height" id="home">
       <h2 class="section__title">Início</h2>
 
-      
+      <div class="form__data">
+         <div class="form__title">Form</div>
+         <form action="">
+            <div class="form__input">
+               <span class="form__label">Nome: </span>
+               <input type="text" placeholder="Nome" required>
+            </div>
+
+            <div class="form__input">
+               <span class="form__label">Sobrenome: </span>
+               <input type="text" placeholder="Sobrenome" required>
+            </div>
+         </form>
+      </div>
    </section>
 
    <!--============== TABELA ============== -->
